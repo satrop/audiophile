961794ae9668d121553a42300642c755be838d0c
satrop <stevenportas@me.com>
2023-01-20 14:01:09 -0800

    About section component built

---
+++ src/components/AboutBlock/AboutBlock.jsx
@@ -0,0 +1,35 @@
+import lg from '/public/assets/shared/desktop/image-best-gear.jpg';
+import md from '/public/assets/shared/tablet/image-best-gear.jpg';
+import sm from '/public/assets/shared/mobile/image-best-gear.jpg';
+
+import './AboutBlocks.scss';
+
+const AboutBlock = () => {
+	return (
+		<section className="about-block ">
+			<div className="about-block__text">
+				<div className="header h2">
+					Bringing you the <span>best</span> audio gear
+				</div>
+				<p>
+					Located at the heart of New York City, Audiophile is the
+					premier store for high end headphones, earphones, speakers,
+					and audio accessories. We have a large showroom and luxury
+					demonstration rooms available for you to browse and
+					experience a wide range of our products. Stop by our store
+					to meet some of the fantastic people who make Audiophile the
+					best place to buy your portable audio equipment.
+				</p>
+			</div>
+			<div className="about-block__image">
+				<picture>
+					<source srcSet={lg} media="(min-width:  90rem)" />
+					<source srcSet={md} media="(min-width:  48rem)" />
+					<img className="border-radius" src={sm} alt="Beast gear" />
+				</picture>
+			</div>
+		</section>
+	);
+};
+
+export default AboutBlock;
--- src/pages/Headphones.jsx
+++ src/pages/Headphones.jsx
@@ -1,5 +1,15 @@
+import AboutBlock from '../components/AboutBlock/AboutBlock';
+import SectionLinks from '../components/SectionLinks/SectionLinks';
+
 const Headphones = () => {
-	return <div>Headphones</div>;
+	return (
+		<main>
+			<div className="content body-flow">
+				<SectionLinks />
+				<AboutBlock />
+			</div>
+		</main>
+	);
 };
 
 export default Headphones;
